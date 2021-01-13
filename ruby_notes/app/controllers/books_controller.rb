72ca3ce2095d868dc02be1c16c278eaf6ea53c12
alanbisbal@gmail.com <alanbisbal@gmail.com>
2021-01-13 07:30:01 -0300

    crud de libro libros

--- ruby_notes/app/controllers/books_controller.rb
+++ ruby_notes/app/controllers/books_controller.rb
@@ -4,19 +4,59 @@ class BooksController < ApplicationController
     @books = Book.where(user_id: current_user.id)
   end
 
+  #display the books of the authenticated user
+
   def show
-    @book = Book.find(params[:id])
-    @notes = @book.notes
+    if current_user.books.exists?(params[:id])
+      @book = current_user.books.find(params[:id])
+      @notes = @book.notes
+    else
+      redirect_to '/'
+    end
   end
 
+  #create a new book for the authenticated user
   def create
+    if !current_user.books.exists?(title: params[:title])
+      current_user.books.create(title: params[:title], user_id: current_user.id)
+      redirect_to '/books'
+    else
+      redirect_back(fallback_location: root_path)
+    end
   end
 
+  #edit a book for the authenticated user
   def edit
+    if current_user.books.exists?(params[:id])
+      @book = current_user.books.find(params[:id])
+    else
+      redirect_to '/'
+    end
   end
 
+  def update
+    @books = current_user.books
+    if @books.exists?(params[:id]) and !@books.exists?(title: params[:book][:title])
+      @book = current_user.books.find(params[:id])
+      @book.update(title: params[:book][:title])
+      redirect_to '/books'
+    else
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  #delete a book and the content for the authenticated user
   def destroy
-
+    if current_user.books.exists?(params[:id])
+      @book = current_user.books.find(params[:id])
+      if @book.title != "Global"
+        @book.destroy
+      end
+    else
+      redirect_to '/'
+    end
+    redirect_to '/books'
   end
+
 
 end
--- ruby_notes/config/routes.rb
+++ ruby_notes/config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :books
+  put 'books/:id', controller: 'books', action: :update
+
+
   resources :notes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
